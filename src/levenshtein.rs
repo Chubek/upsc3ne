9eee07872c3e4c4f6c98560b55f9695853abefb4
Chubak <chubakbiddpaa@gmail.com>
2022-02-16 18:12:26 +0330

    new stuff

--- src/levenshtein.rs
+++ src/levenshtein.rs
@@ -1,8 +1,35 @@
-use std::cmp::min;
+use std::cmp::{max, min};
+use regex::Regex;
+use lazy_static::lazy_static;
+use std::collections::HashSet;
+
+fn return_larger_smaller<'a>(a: &'a String, b: &'a String) -> (&'a String, &'a String) {
+    let len_a = a.len() as i32;
+    let len_b = b.len() as i32;
+
+    let diff = len_a - len_b;
+
+    let larger: &String;
+    let smaller: &String;
+
+    if diff >= 0 {
+        larger = a;
+        smaller = b;
+    } else {
+        larger = b;
+        smaller = a;
+    }
+
+    return (larger, smaller)
+
+
+}
 
 pub fn levenshtein(a_str: &String, b_str: &String) -> u32 {
-    let a = a_str.as_bytes();
-    let b= b_str.as_bytes();
+    let larger_smaller = return_larger_smaller(a_str, b_str);
+
+    let a = larger_smaller.1.as_bytes();
+    let b = larger_smaller.0.as_bytes();
 
     let m = a.len();
     let n = b.len();
@@ -31,5 +58,64 @@ pub fn levenshtein(a_str: &String, b_str: &String) -> u32 {
 
     }
 
-    return d[m - 1usize][n - 1usize]
+    return d[m - 1usize][n - 1usize] + 1
+}
+
+pub fn lev_ratio(a: &String, b: &String) -> u32 {
+    let d = levenshtein(a, b);
+
+    let m = a.len() as u32;
+    let n = b.len() as u32;
+
+    let max = max(m, n);
+
+    let ratio = (d as f32) / (max as f32) * 100f32;
+
+    println!("{}", ratio);
+
+    return ratio as u32
+}
+
+
+pub fn token_set_ratio(a: &String, b: &String) -> u32 {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"[.,/#!?$%\^&\*;:{}=\-_`~()\[\]]").unwrap();
+        static ref RESPACE: Regex = Regex::new(r"\s+").unwrap();
+    }
+
+    let a_lower = a.to_lowercase();
+    let b_lower = b.to_lowercase();
+    let a_trim = a_lower.trim();
+    let b_trim = b_lower.trim();
+
+    let a_rep = RE.replace_all(a_trim, "");
+    let b_rep = RE.replace_all(b_trim, "");
+
+    let a_split = RESPACE.split(&a_rep);
+    let b_split = RESPACE.split(&b_rep);
+
+    let a_set: HashSet<&str> = HashSet::from_iter(a_split);
+    let b_set: HashSet<&str> = HashSet::from_iter(b_split);
+
+    let a_b_intersect = a_set.intersection(&b_set);
+    let a_diff_b = a_set.difference(&b_set);
+    let b_diff_a = b_set.difference(&a_set);
+
+    let mut sect_str = a_b_intersect
+        .into_iter()
+        .collect::<Vec<_>>();
+    sect_str.sort();
+
+    let mut sect_diff_ab = a_diff_b
+        .into_iter()
+        .collect::<Vec<_>>();
+    sect_str.sort();
+
+    let mut sect_diff_ba = b_diff_a
+        .into_iter()
+        .collect::<Vec<_>>();
+    sect_str.sort();
+
+    println!("{:#?}\n{:#?}\n{:#?}", sect_str, sect_diff_ab, sect_diff_ba);
+    return 0
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod levenshtein;
 
+
 #[test]
 fn test_levenshtein() {
     let res = levenshtein::levenshtein(&String::from("sa11a"), &String::from("saa1"));
@@ -7,7 +8,20 @@ fn test_levenshtein() {
     println!("{:#?}", res)
 }
 
+#[test]
+fn test_token_set() {
+    let res = levenshtein::token_set_ratio(&String::from(" Zebra A good day"), &String::from("A bad zebra day" ));
 
+    println!("{:#?}", res)
+}
+
+
+#[test]
+fn test_ratio() {
+    let res = levenshtein::lev_ratio(&String::from("Zxtfwu:?1!"), &String::from("uxa"));
+
+    println!("{:#?}", res)
+}
 
 
 fn main() {
